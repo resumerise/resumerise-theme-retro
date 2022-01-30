a9002aa25db558e31f61e04f6f6bea9861b3247e
Elton M <eltonmarku@gmail.com>
2022-01-30 22:32:23 +0100

    add logos

--- meta.ts
+++ meta.ts
@@ -3,7 +3,6 @@ import { ResumeriseMeta } from "resumerise_library/mod.ts";
 export const meta = {
   colors: ["#000", "#fff"],
   description: "A starter template for resumerise.",
-  name: "resumerise_theme_retro",
   platforms: ["DESKTOP", "INTERACTIVE", "MOBILE", "PRINT"],
   tags: ["starter", "blueprint", "retro"],
   categories: ["EDUCATION"],
